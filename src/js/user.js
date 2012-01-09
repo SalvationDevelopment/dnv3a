fb396e605422faff6b8b1274f03bbb9756c034fb
Simon Lindholm <simon.lindholm10@gmail.com>
2012-01-09 23:37:32 +0100

    Make it possible internally to add/remove friends.
    And make user management work.

--- src/js/user.js
+++ src/js/user.js
@@ -9,6 +9,10 @@ window.User = Class.extend({
 	init: function(name, mod) {
 		this.name = name;
 		this.modStatus = mod;
+	},
+
+	isFriend: function() {
+		return Friends.isFriend(this.name);
 	}
 });
 
@@ -32,7 +36,7 @@ window.Users = {
 	},
 
 	getUser: function(name) {
-		return map[',' + name] || null;
+		return this.map[',' + name] || null;
 	},
 
 	handleMessage: function(ev, data) {
@@ -44,7 +48,8 @@ window.Users = {
 			}
 			return true;
 		}
-		else if (ev === 'Offline users') {
+
+		if (ev === 'Offline users') {
 			for (var i = 0; i < data.length; ++i) {
 				var name = data[i];
 				var user = this.map[',' + name];
@@ -55,9 +60,18 @@ window.Users = {
 		}
 	},
 
+	sendCurrentInfo: function(handler) {
+		var cb = this.cbMap[handler];
+		for (var ind in this.map) {
+			var user = this.map[ind];
+			cb('online', user);
+		}
+	},
+
 	dispatch: function() {
-		for (var cb in cbMap) {
-			cbMap[cb].call(arguments);
+		for (var cb in this.cbMap) {
+			var cb = this.cbMap[cb];
+			cb.apply(cb, arguments);
 		}
 	}
 };
@@ -85,33 +99,82 @@ window.Friends = {
 
 	handleMessage: function(ev, data) {
 		if (ev === 'Friends') {
-			if (this.loaded) {
-				console.warn("Friend data gotten twice.");
-				return;
-			}
+			console.assert(!this.loaded);
 			this.loaded = true;
-
-			this.friends = data.slice();
-			for (var i = 0; i < data.length; ++i) {
-				var name = data[i];
-				this.map[',' + name] = 1;
-				var user = Users.getUser(name);
-				if (user) this.dispatch('online', user);
-			}
+			this.friends = [];
 
 			var that = this;
+			data.forEach(function(name) {
+				that.doAddFriend(name);
+			});
+
 			Users.listen(function(ev, user) {
 				if (that.isFriend(user.name)) {
+					// Pass online/offline signals regarding friends on.
 					that.dispatch(ev, user);
 				}
 			});
 			return true;
 		}
+
+		if (ev === 'Add friend') {
+			this.doAddFriend(data[0]);
+			return true;
+		}
+		if (ev === 'Delete friend') {
+			this.doRemoveFriend(data[0]);
+			return true;
+		}
+	},
+
+	doAddFriend: function(name) {
+		console.assert(!this.map[',' + name]);
+		this.friends.push(name);
+		this.map[',' + name] = 1;
+
+		this.dispatch('friend', name);
+		var user = Users.getUser(name);
+		if (user) this.dispatch('online', user);
+	},
+
+	doRemoveFriend: function(name) {
+		var ind = this.friends.indexOf(name);
+		console.assert(ind !== -1);
+		this.friends.splice(ind, 1);
+		delete this.map[',' + name];
+
+		var user = Users.getUser(name);
+		if (user) this.dispatch('offline', user);
+		this.dispatch('unfriend', name);
+	},
+
+	addFriend: function(name) {
+		Communicator.send(['Add friend', name]);
+		// We will shortly receive either an 'Add friend' signal back, or one
+		// of the following 'Async error's:
+		// - ["<user> is not a registered user"]
+		// - ["<user> is already your friend"]
+	},
+
+	removeFriend: function(name) {
+		Communicator.send(['Delete friend', name]);
+		// We will shortly receive either an 'Delete friend' signal back, or
+		// an 'Async error' with data ["<user> is not your friend"].
+	},
+
+	sendCurrentInfo: function(handler) {
+		var cb = this.cbMap[handler];
+		this.friends.forEach(function(name) {
+			cb('friend', name);
+			var user = Users.getUser(name);
+			if (user) cb('online', user);
+		});
 	},
 
 	dispatch: function() {
-		for (var cb in cbMap) {
-			cbMap[cb].call(arguments);
+		for (var handler in this.cbMap) {
+			var cb = this.cbMap[handler];
+			cb.apply(cb, arguments);
 		}
 	}
 };
